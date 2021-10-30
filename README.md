3399b360dab50460fe6c55652d5ca820c6b3340a
nobody <23215696+taicaile@users.noreply.github.com>
2021-10-30 22:35:29 +0800

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# init.sh
+
+This script is used to initialize Linux development environment.
+
+Currently, this script do the followings:
+
+- evaluate `direnv`
+- initialize bash prompt
--- init.sh
+++ init.sh
@@ -6,3 +6,27 @@ eval "$(direnv hook bash)"
 # update history size
 HISTSIZE=2000
 HISTFILESIZE=20000
+
+# bash prompt theme
+# shellcheck disable=SC2016,SC1004
+__bash_prompt() {
+    local userpart='`export XIT=$? \
+        && [ ! -z "${GITHUB_USER}" ] && echo -n "\[\033[0;32m\]@${GITHUB_USER} " || echo -n "\[\033[0;32m\]\u " \
+        && [ "$XIT" -ne "0" ] && echo -n "\[\033[1;31m\]➜" || echo -n "\[\033[0m\]➜"`'
+    local gitbranch='`\
+        export BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null); \
+        if [ "${BRANCH}" != "" ]; then \
+            echo -n "\[\033[0;36m\](\[\033[1;31m\]${BRANCH}" \
+            && if git ls-files --error-unmatch -m --directory --no-empty-directory -o --exclude-standard ":/*" > /dev/null 2>&1; then \
+                    echo -n " \[\033[1;33m\]✗"; \
+               fi \
+            && echo -n "\[\033[0;36m\]) "; \
+        fi`'
+    local lightblue='\[\033[1;34m\]'
+    local removecolor='\[\033[0m\]'
+    PS1="${userpart} ${lightblue}\w ${gitbranch}${removecolor}\$ "
+    unset -f __bash_prompt
+}
+
+__bash_prompt
+export PROMPT_DIRTRIM=4
