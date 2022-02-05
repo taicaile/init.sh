e44aae7fbdbaff786a1707b1181e365efae1f5e3
Neil <mailto:23215696+taicaile@users.noreply.github.com>
2022-02-05 16:33:42 +0800

    convert tab to 4 spaces

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -4,5 +4,12 @@
 # info "isntall mysql"
 # sudo apt -q install -y --no-install-recommends mysql-server
 
+# sudo apt install nvidia-cuda-toolkit # the nvidia driver will be replaced.
 
-sudo apt install nvidia-cuda-toolkit
+# google-chrome
+
+# telegram
+
+sudo apt -q install -y --no-install-recommends \
+    anki \
+    copyq
