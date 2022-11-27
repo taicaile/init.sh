327573289414536a3ce60ccc7d5ba138c78f76fa
taicaile <23215696+taicaile@users.noreply.github.com>
2022-11-27 19:20:41 +0800

    update init.sh

--- DOTFILES/init/init.sh
+++ DOTFILES/init/init.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# 交互式模式的初始化脚本
+# 防止被加载两次
+if [ -z "$_INIT_SH_LOADED" ]; then
+    _INIT_SH_LOADED=1
+else
+    return
+fi
+
+# 如果是非交互式则退出，比如 bash test.sh 这种调用 bash 运行脚本时就不是交互式
+# 只有直接敲 bash 进入的等待用户输入命令的那种模式才成为交互式，才往下初始化
+case "$-" in
+    *i*) ;;
+    *) return
+esac
+
 appendpath () {
     case ":$PATH:" in
         *:"$1":*)
