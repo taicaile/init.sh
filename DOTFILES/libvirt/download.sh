34516ba643d2af62a12221db7f3ae3b1e0bf3d59
local.local <local@local.local>
2022-12-12 15:40:50 +0800

    add libvirt download pages

---
+++ DOTFILES/libvirt/download.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "download Windows 10 page,"
+echo "https://www.microsoft.com/en-us/software-download/windows10ISO"
+
+echo "virt-win download page,"
+echo "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso"
+
+echo "spice guest tools download page,"
+echo "https://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-latest.exe"
