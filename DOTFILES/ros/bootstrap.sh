efad01caf2835717e74d401277a9b4b5982d2b99
local.local <local@local.local>
2022-12-12 16:38:15 +0800

    add ros

---
+++ DOTFILES/ros/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "http://wiki.ros.org/Installation/Ubuntu"
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt install curl # if you haven't already installed curl
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt install ros-noetic-desktop-full
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
