36f9668327e8ac3c4c0d6f3588c388c21229ddf2
local.local <local@local.local>
2022-12-13 10:54:48 +0800

    update webdav

--- DOTFILES/webdav/bootstrap.sh
+++ DOTFILES/webdav/bootstrap.sh
@@ -10,3 +10,22 @@ DOWNLOAD_URL=$(curl -s https://api.github.com/repos/hacdias/webdav/releases/late
 echo "$DOWNLOAD_URL"
 
 # curl -s -L --create-dirs -o .download "$DOWNLOAD_URL"
+
+cat <<'END'
+On windows, you need to use the following url to connect the server,
+
+Open file browser, type http://server-ip:8080/ , then type the user name and password
+
+Or, right click the `This PC`, click the `Map network drive...`, then add the url,
+
+http://server-ip:8080
+
+or,
+\\server-ip@8080\
+
+You may also need to check the `Connect using different credentials` if auth is required.
+
+On Ubuntu, you can use the
+dav://server-ip:8080/
+
+END
