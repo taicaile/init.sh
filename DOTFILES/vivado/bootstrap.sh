0b4f240176d818ee63b19c6c9b56a52e06e7b74a
local.local <local@local.local>
2022-12-19 09:00:58 +0800

    add vivado

---
+++ DOTFILES/vivado/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install libncurses5
+sudo apt install libtinfo5
+sudo apt install libncurses5-dev libncursesw5-dev
+sudo apt install ncurses-compat-libs
